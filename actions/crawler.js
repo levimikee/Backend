50d04a729aaf0a1c26a82dda8632238b02c18359
Bensagra <bensagra@gmail.com>
2025-07-28 12:57:17 -0300

    fix: change body to data in getHtmlContent for axios request

--- actions/crawler.js
+++ actions/crawler.js
@@ -145,7 +145,7 @@ class Crawler {
           // 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
           Authorization: authHeader
         },
-        body: {
+        data: {
           "source": "universal",
           "render": "html",
           "url": url,
